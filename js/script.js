097880403ff3a8e6f52733e860aae1d930b91088
nuke92 <juhasz.krisztina1992@gmail.com>
2021-03-07 13:46:20 +0100

    put the header back in place

--- js/script.js
+++ js/script.js
@@ -1,10 +1,6 @@
-// function dayHover() {
-//     document.getElementById("day1").classList.toggle("active");
-// }
-
-$(function () {
-    $("#header").load("header.html");
-});
+// $(function () {
+//     $("#header").load("header.html");
+// });
 
 $(function () {
     $("#nav").load("nav.html");
